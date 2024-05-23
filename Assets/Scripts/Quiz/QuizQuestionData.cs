05a11beb11fbd0277b32b9cfb113b5a1141ce410
DarksiderRay <NG0007UI@e.ntu.edu.sg>
2024-05-24 00:16:26 +0800

    - feat: Added Quiz Manager UI and script (WIP)

---
+++ Assets/Scripts/Quiz/QuizQuestionData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuizQuestionData", menuName = "Audio School XR/Quiz Question Data", order = 1)]
+public class QuizQuestionData : ScriptableObject
+{
+    public string question;
+    public string correctAnswer;
+    public List<string> wrongAnswers;
+}
