1a18e37d6c849450068f507ea660019f78c1ad7a
Skizzle <74311119+Itspossible100pct@users.noreply.github.com>
2024-05-28 13:39:28 +0200

    Added Obi Collider to small studio equipment and enabled collisions on Obi Rope componenent.

---
+++ Assets/Scripts/GameOptions/RoomSceneGrabber.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSceneGrabber : MonoBehaviour
+{
+    private OVRSceneManager _oVRSceneManager;
+    private OVRSceneRoom _sceneRoom;
+    private OVRScenePlane[] _roomWalls;
+    
+    
+    
+    
+}
