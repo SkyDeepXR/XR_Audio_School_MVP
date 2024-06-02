f39fba6f779672ff803ebf400c84acefb1154edb
Skizzle <74311119+Itspossible100pct@users.noreply.github.com>
2024-06-02 14:14:00 +0200

    Fixed the transition from Onboarding to Module 1A.  Repositioned connectors and added Highlight feature. All in MainScene- v2 Final
    Also added Obi Colliders to Equipment and table in NightClub and updated that prefab.

--- Assets/Scripts/ResetCanvasPosition.cs
+++ Assets/Scripts/ResetCanvasPosition.cs
@@ -1,26 +1,26 @@
 using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.XR;
 
 public class ResetCanvasPosition : MonoBehaviour
 {
     public Transform cameraTransform;  // Assign your XR camera's transform here in the inspector
     public float distanceFromCamera = 1.25f;  // How far in front of the camera the Canvas should appear
+    public float yOffset = 0f;  // Y-axis offset for the canvas position
 
     [SerializeField] private Transform _companionCanvas1;
     [SerializeField] private Transform _companionCanvas2;
     public Vector3 companionOffset1 = new Vector3(0f, 0f, 0f);  // Example offset
     public Vector3 companionOffset2 = new Vector3(0f, 0f, 0f); // Example offset
-    
+
     private IEnumerator Start()
     {
         // Wait for a short delay before setting the canvas position
         yield return new WaitForSeconds(0.1f);  // Delay for 1 second, adjust as needed
         ResetCanvas();
     }
-    
+
 
     void Update()
     {
@@ -32,8 +32,9 @@ public class ResetCanvasPosition : MonoBehaviour
 
     private void ResetCanvas()
     {
-        // Position the Canvas at the camera's position (at eye level)
-        transform.position = cameraTransform.position + cameraTransform.forward * distanceFromCamera;
+        // Position the Canvas at the camera's position with an offset on the Y-axis
+        Vector3 positionOffset = cameraTransform.forward * distanceFromCamera + new Vector3(0, yOffset, 0);
+        transform.position = cameraTransform.position + positionOffset;
 
         // Rotate the Canvas to face directly towards the camera
         Vector3 toCamera = (cameraTransform.position - transform.position).normalized;
