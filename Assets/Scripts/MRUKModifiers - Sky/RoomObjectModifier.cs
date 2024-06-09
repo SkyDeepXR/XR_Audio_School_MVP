024e9bc0607dcd9456c7280b59db06dbfb54c9ab
Skizzle <74311119+Itspossible100pct@users.noreply.github.com>
2024-06-09 10:28:56 +0200

    Fixed hand posed grabs to a good degree;  Prepped for hand poke UI and hand menu.

--- Assets/Scripts/MRUKModifiers - Sky/RoomObjectModifier.cs
+++ Assets/Scripts/MRUKModifiers - Sky/RoomObjectModifier.cs
@@ -6,7 +6,7 @@ using Obi;
 
 public class RoomObjectModifier : MonoBehaviour
 {
-    
+    public ObiCollisionMaterial frictionMaterial;
 
     public void GetRoomObjectAndDestroy()
     {
@@ -27,7 +27,6 @@ public class RoomObjectModifier : MonoBehaviour
         Destroy(obj);
     }
 
-    
     public void ToggleMeshRenderersInMRUKRoom(bool enableRenderers)
     {
         MRUKRoom mrukComponent = FindObjectOfType<MRUKRoom>();
@@ -48,6 +47,30 @@ public class RoomObjectModifier : MonoBehaviour
             }
             // Recursively toggle renderers in deeper child objects
             ToggleMeshRenderers(child.gameObject, enableRenderers);
+        }
+    }
+
+    public void AddObiCollidersToRoomObjects()
+    {
+        MRUKRoom mrukComponent = FindObjectOfType<MRUKRoom>();
+        if (mrukComponent != null)
+        {
+            AddObiColliders(mrukComponent.gameObject);
+        }
+    }
+
+    private void AddObiColliders(GameObject obj)
+    {
+        foreach (Transform child in obj.transform)
+        {
+            Collider collider = child.GetComponent<Collider>();
+            if (collider != null)
+            {
+                ObiCollider obiCollider = child.gameObject.AddComponent<ObiCollider>();
+                obiCollider.CollisionMaterial = frictionMaterial;
+            }
+            // Recursively add ObiColliders in deeper child objects
+            AddObiColliders(child.gameObject);
         }
     }
 }
